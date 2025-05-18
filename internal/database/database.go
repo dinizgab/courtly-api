0fc2e4174e2ac2d20fe752ee9f1bb6e392ea75f2
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-05-18 02:09:27 -0300

    feat: create database interface

---
+++ internal/database/database.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/dinizgab/booking-mvp/internal/config"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type Database interface {
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, arguments ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, arguments ...any) pgx.Row
+}
+
+type databaseImpl struct {
+	conn *pgx.Conn
+}
+
+func New(config *config.DBConfig) (Database, error) {
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, config.DBUrl)
+	if err != nil {
+		return nil, fmt.Errorf("Database.New: %w", err)
+	}
+
+	if err := conn.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("Database.New: %w", err)
+	}
+
+	return &databaseImpl{
+		conn: conn,
+	}, nil
+}
+
+func (d *databaseImpl) Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error) {
+	return d.conn.Exec(ctx, sql, arguments...)
+}
+
+func (d *databaseImpl) Query(ctx context.Context, sql string, arguments ...any) (pgx.Rows, error) {
+	return d.conn.Query(ctx, sql, arguments...)
+}
+
+func (d *databaseImpl) QueryRow(ctx context.Context, sql string, arguments ...any) pgx.Row {
+	return d.conn.QueryRow(ctx, sql, arguments...)
+}
