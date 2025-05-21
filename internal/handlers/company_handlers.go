efe3d35697c9ca64ee5047ab436ffd055837fb6b
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-05-21 13:10:56 -0300

    fix: add error logging

--- internal/handlers/company_handlers.go
+++ internal/handlers/company_handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"log"
+
 	"github.com/dinizgab/booking-mvp/internal/entity"
 	"github.com/dinizgab/booking-mvp/internal/usecase"
 	"github.com/gin-gonic/gin"
@@ -10,12 +12,14 @@ func CreateNewCompany(uc usecase.CompanyUsecase) func(*gin.Context) {
 	return func(c *gin.Context) {
 		var company entity.Company
 		if err := c.ShouldBindJSON(&company); err != nil {
+			log.Println(err)
 			c.JSON(400, gin.H{"error": "Invalid request"})
 			return
 		}
 
 		err := uc.Create(c.Request.Context(), company)
 		if err != nil {
+			log.Println(err)
 			c.JSON(500, gin.H{"error": "Failed to create company"})
 			return
 		}
@@ -32,17 +36,17 @@ func LoginCompany(uc usecase.CompanyUsecase) func(*gin.Context) {
 		}
 
 		if err := c.ShouldBindJSON(&input); err != nil {
+			log.Println(err)
 			c.JSON(400, gin.H{"error": "Invalid request"})
 			return
 		}
 
 		err := uc.Login(c.Request.Context(), input.Email, input.Password)
 		if err != nil {
+			log.Println(err)
 			if err == entity.ErrInvalidCredentials {
 				c.JSON(401, gin.H{"error": "Invalid credentials"})
 			}
-
-
 			return
 		}
 
