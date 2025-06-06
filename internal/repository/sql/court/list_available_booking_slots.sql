f5254d06b96e71a7fe21339a30fa5e3ad593dc2c
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-06-06 19:35:55 -0300

    feat: list pending bookings as unavailable

--- internal/repository/sql/court/list_available_booking_slots.sql
+++ internal/repository/sql/court/list_available_booking_slots.sql
@@ -1,3 +1,5 @@
 select start_time, end_time
 from bookings
-where court_id = $1 and date(start_time) = date($2) and status != 'cancelled'
+where court_id = $1
+    and date(start_time) = date($2)
+    and status not in ('cancelled', 'pending')
