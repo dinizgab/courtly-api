adb7e2e5641d5cc25bae707effa6110d853612e1
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-06-09 10:58:36 -0300

    feat: create court photo entity

--- internal/entity/court.go
+++ internal/entity/court.go
@@ -3,12 +3,12 @@ package entity
 import "time"
 
 type Court struct {
-	ID            string    `json:"id"`
-	CompanyId     string    `json:"company_id"`
-	Name          string    `json:"name"`
-	IsActive      bool      `json:"is_active"`
-	SportType     string    `json:"sport_type"`
-    // TODO - Change to int64 for cents
+	ID        string `json:"id"`
+	CompanyId string `json:"company_id"`
+	Name      string `json:"name"`
+	IsActive  bool   `json:"is_active"`
+	SportType string `json:"sport_type"`
+	// TODO - Change to int64 for cents
 	HourlyPrice   float64   `json:"hourly_price"`
 	Description   string    `json:"description"`
 	Capacity      int       `json:"capacity"`
@@ -16,5 +16,13 @@ type Court struct {
 	ClosingTime   time.Time `json:"closing_time"`
 	BookingsToday int       `json:"bookings_today"`
 	Bookings      []Booking `json:"bookings"`
-    Company       *Company  `json:"company,omitempty"`
+	Company       *Company  `json:"company,omitempty"`
+}
+
+type CourtPhoto struct {
+	ID       string `json:"id"`
+	CourtId  string `json:"court_id"`
+	Path     string `json:"path"`
+	Position int    `json:"position"`
+	IsCover  bool   `json:"is_cover"`
 }
